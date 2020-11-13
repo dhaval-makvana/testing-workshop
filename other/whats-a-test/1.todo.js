079ac21aefe55c279338fa6ada08ecbbdc98cc3c
dhaval-nykaa <dhavalkumar.makvana@nykaa.com>
2020-11-13 12:31:03 +0530

    chore: 1.todo.js

--- other/whats-a-test/1.todo.js
+++ other/whats-a-test/1.todo.js
@@ -19,5 +19,21 @@ error if the subtract function were to have a bug
 */
 
 // sum is intentionally broken so you can see errors in the tests
-const sum = (a, b) => a - b
+const sum = (a, b) => a + b
 const subtract = (a, b) => a - b
+
+const result = sum(3, 7);
+const expected = 10;
+if (result !== expected) {
+  throw new Error(
+    `The result of ${result} is not equal to expected of ${expected}`
+  )
+}
+
+const resultSub = subtract(7, 3);
+const expectedSub = 4;
+if (resultSub !== expectedSub) {
+  throw new Error(
+    `The result of ${resultSub} is not equal to expected of ${expectedSub}`
+  )
+}
