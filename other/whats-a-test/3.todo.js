301dfcda82e72379a3a92c12a501d58f15ecc4d8
dhaval-nykaa <dhavalkumar.makvana@nykaa.com>
2020-11-13 15:11:27 +0530

    chore: coding a testing framework

--- other/whats-a-test/3.todo.js
+++ other/whats-a-test/3.todo.js
@@ -29,13 +29,17 @@ const {sum, subtract} = require('./math')
 
 let result, expected
 
-result = sum(3, 7)
-expected = 10
-expect(result).toBe(expected)
+test('testing sum function', () => {
+  result = sum(3, 7)
+  expected = 10
+  expect(result).toBe(expected)
+});
 
-result = subtract(7, 3)
-expected = 4
-expect(result).toBe(expected)
+test('testing subtract function', () => {
+  result = subtract(7, 3)
+  expected = 4
+  expect(result).toBe(expected)
+});
 
 function expect(actual) {
   return {
@@ -44,5 +48,14 @@ function expect(actual) {
         throw new Error(`${actual} is not equal to ${expected}`)
       }
     },
+  }
+}
+
+function test(title, callback) {
+  try {
+    callback();
+    console.log(`✅   ${title}`); // important that the log is after cb.
+  } catch (error) {
+    console.log(`❌   ${error}`);
   }
 }
